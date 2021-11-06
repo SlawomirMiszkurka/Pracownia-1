3eb1387f764ffdec1d5fd964b014e7fc7d3be2c3
Sławomir Miszkurka <93028338+SlawomirMiszkurka@users.noreply.github.com>
2021-11-07 00:15:06 +0100

    Delete Zad 19.py

--- Zad 19.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Podaj długość pierwszego boku:"))
-b = int(input("Podaj długość drugiego boku:"))
-c = int(input("Podaj długość trzeciego boku:"))
-s = (a + b + c) / 2
-area = (s * (s - a) * (s - b) * (s - c))**(1/2)
-print(f"Area: {area}")
