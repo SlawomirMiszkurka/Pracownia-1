66ca793d4a8b23a443603cd72265795f9ede2f02
Sławomir Miszkurka <93028338+SlawomirMiszkurka@users.noreply.github.com>
2021-11-07 00:11:09 +0100

    Delete Zad 15.py

--- Zad 15.py
+++
@@ -1,4 +0,0 @@
-temperatura = int(input("Podaj temperaturę :"))
-Fahrenheit = (temperatura * 1.8) + 32
-Kelvin = temperatura +273.15
-print(f'" {temperatura} stopni celcjusza to {Fahrenheit} stopni Fahrenheit i {Kelvin} stopni Kelvina"')
