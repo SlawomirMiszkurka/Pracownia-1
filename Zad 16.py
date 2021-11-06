a2f529c0f69c6332b6154057ead5ba15a6adc278
Sławomir Miszkurka <93028338+SlawomirMiszkurka@users.noreply.github.com>
2021-11-07 00:11:16 +0100

    Delete Zad 16.py

--- Zad 16.py
+++
@@ -1,8 +0,0 @@
-print("odpowiedź a: ",15 * 38)
-print("odpowiedź b: ", (3+4)*(5+9))
-print("odpowiedź c: ", 7//2)
-print("odpowiedź d: ", 48%5)
-print("odpowiedź e: ", (8+7+4+2)/4)
-print("odpowiedź f: ", 2**10)
-print("odpowiedź g: ", int(49**0.5))
-print("odpowiedź h: ", int(80*0.25))
