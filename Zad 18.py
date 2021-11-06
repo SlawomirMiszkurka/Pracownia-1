9cdbba0775469535d4cde1300bd8f76cd56f717d
Sławomir Miszkurka <93028338+SlawomirMiszkurka@users.noreply.github.com>
2021-11-07 00:11:41 +0100

    Delete Zad 18.py

--- Zad 18.py
+++
@@ -1,4 +0,0 @@
-wzrost = int(input("Podaj swój wzrost w cm:"))
-stopy = int(wzrost * 0.032808)
-cale = int(wzrost *0.39370- stopy *12)
-print(f'"Masz {wzrost}cm wzrostu to {stopy} stóp i {cale} cali"')
